bd99d22fc3428554b5f63d3de6bceb77c33d7076
Gurpal Singh <gillgurpal699@gmail.com>
2022-05-23 16:35:07 +0530

    add meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -16,6 +16,23 @@ export default class _Document extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Cantarell&family=Roboto&display=swap" rel="stylesheet" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="theme-color" content={'#' + Math.floor(Math.random() * 16777215).toString(16)} />
+          <meta name="description" content="An Object Oriented Discord Api Wrapper for Deno." />
+          <meta property='og:title' content='DenoCord' />
+          <meta property='og:description' content='An Object Oriented Discord Api Wrapper for Deno.' />
+          <meta property='og:image' content='/images/denocord.png' />
+          <meta property='og:url' content='https://denocord-docs.vercel.app/' />
+          <meta property='og:type' content='website' />
+          <meta property='og:site_name' content='DenoCord' />
+          <meta property='og:locale' content='en_US' />
+          <meta property='og:locale:alternate' content='en_US' />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="DenoCord" />
+          <meta name="twitter:description" content="An Object Oriented Discord Api Wrapper for Deno." />
+          <meta name="twitter:image" content="/images/denocord.png" />
+          <meta name="twitter:image:alt" content="DenoCord" />
+
         </Head>
         <body>
           <Main />
