e3c445e5c11c4124781a2c12af36d38be4b69377
Gurpal Singh <gillgurpal699@gmail.com>
2022-06-13 13:53:24 +0530

    Revert "fix assets prefix"
    This reverts commit a505fbc00d643c6df1cf8b4f1a15bb0b0ff0433c.

--- next.config.js
+++ next.config.js
@@ -2,5 +2,4 @@ const { withContentlayer } = require('next-contentlayer')
 /**
  * @type {import("next").NextConfig}
  */
-const isProd = process.env.NODE_ENV === "production"
-module.exports = withContentlayer({ reactStrictMode: true, assetPrefix: isProd ? "/documentation" : "" })
+module.exports = withContentlayer({ reactStrictMode: true, assetPrefix: "/documentation/", basePath: "/documentation/" })
