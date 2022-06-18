80573d7c47b7871dbe91a0345535c67552e9cf9a
Gurpal Singh <gillgurpal699@gmail.com>
2022-06-18 13:41:39 +0530

    remove trailing slash from `assetPrefix`

--- next.config.js
+++ next.config.js
@@ -3,4 +3,4 @@ const isProd = process.env.NODE_ENV === "production"
 /**
  * @type {import("next").NextConfig}
  */
-module.exports = withContentlayer({ reactStrictMode: true, assetPrefix: isProd ? "https://DenoCord-labs.github.io/documentation/" : "", basePath: isProd ? "/documentation" : "" })
+module.exports = withContentlayer({ reactStrictMode: true, assetPrefix: isProd ? "https://denocord-labs.github.io/documentation" : "", basePath: isProd ? "/documentation" : "" })
